0387d4e02ad55e0a65d94590bf65d8db8222caf9
Jeffrey Leary <None>
2018-04-08 17:43:26 -0400

    prepared for PyPi

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+
 setup(
   name = 'piwifi',
   packages = ['piwifi'],
@@ -8,10 +9,4 @@ setup(
   author_email = 'sillymonkeysoftware@gmail.com',
   url = 'https://github.com/jeffleary00/piwifi',
   download_url = 'https://github.com/jeffleary00/piwifi/tarball/0.1',
-  classifiers = [
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3',
-  ],
-  keywords = ['wpa_supplicant', 'raspberry pi', 'linux'], 
 )
