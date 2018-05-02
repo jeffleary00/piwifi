40a15524a84856c71c15cbc92cfd2d733aabcb75
Jeffrey Leary <None>
2018-05-02 12:46:16 -0400

    skipped 0.2, jump to 0.3 due to commit/tag error.

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'piwifi',
   packages = ['piwifi'],
-  version = '0.2',
+  version = '0.3',
   description = 'Manage wpa_supplicant wifi networks on Raspberry Pi',
   author = 'Jeff Leary',
   author_email = 'sillymonkeysoftware@gmail.com',
   url = 'https://github.com/jeffleary00/piwifi',
-  download_url = 'https://github.com/jeffleary00/piwifi/tarball/0.2',
+  download_url = 'https://github.com/jeffleary00/piwifi/tarball/0.3',
   classifiers = [],
   keywords = ['wpa_supplicant', 'raspberry pi', 'linux'], 
 )
